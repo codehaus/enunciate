03c66119b9adb1067ad8e8bee61800643259d940
stoicflame <stoicflame@c2b6a248-b21b-0410-9d80-82131e002806>
2007-04-13 20:01:52 +0000

    A complete Enunciate packaging plugin, finished.

---
+++ maven-enunciate-plugin/src/main/java/org/codehaus/enunciate/GenerateMojo.java
@@ -0,0 +1,46 @@
+package org.codehaus.enunciate;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.enunciate.main.Enunciate;
+
+import java.util.Properties;
+
+/**
+ * Goal which performs the "generate" step of an Enunciate build process.
+ *
+ * @goal generate
+ * @phase generate-sources
+ * @requiresDependencyResolution runtime
+ */
+public class GenerateMojo extends AbstractMojo {
+
+  /**
+   * The Maven project reference.
+   *
+   * @parameter expression="${project}"
+   * @required
+   * @readonly
+   */
+  protected MavenProject project;
+
+  public void execute() throws MojoExecutionException {
+    Properties properties = this.project.getProperties();
+    Enunciate.Stepper stepper = (Enunciate.Stepper) properties.get(ConfigMojo.ENUNCIATE_STEPPER_PROPERTY);
+    if (stepper == null) {
+      throw new MojoExecutionException("No stepper found in the project!");
+    }
+
+    if (stepper.getNextTarget() == null) {
+      throw new MojoExecutionException("Uninitialized or stale Enunciate stepper.");
+    }
+
+    try {
+      stepper.stepTo(Enunciate.Target.GENERATE);
+    }
+    catch (Exception e) {
+      throw new MojoExecutionException("Problem with generate step.", e);
+    }
+  }
+}
