c3dd751e00dc77430c52a2708d2745372a9f9b4e
stoicflame <stoicflame@c2b6a248-b21b-0410-9d80-82131e002806>
2011-03-26 20:07:50 +0000

    checking initial project

---
+++ enunciate-501-parent/enunciate-501-client/src/main/java/org/codehaus/enunciate/samples/App.java
@@ -0,0 +1,13 @@
+package org.codehaus.enunciate.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ enunciate-501-parent/enunciate-501-webapp/src/main/java/org/codehaus/enunciate/samples/ws/BogusService.java
@@ -0,0 +1,14 @@
+package org.codehaus.enunciate.samples.ws;
+
+import javax.jws.WebService;
+
+/**
+ * @author Ryan Heaton
+ */
+@WebService
+public class BogusService {
+
+  public String doSomethingBogus(String param1) {
+    return null;
+  }
+}
